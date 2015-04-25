a8aec334f2a92d0da25598f6314c87ada966b937
Eugene Sokolenko <eugene@Eugenes-iMac.local>
2015-04-25 10:24:11 +0300

    initial commit

---
+++ WeatherTestTask/WeatherTestTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherTestTask
+//
+//  Created by Eugene Sokolenko on 25.04.15.
+//  Copyright (c) 2015 Eugene Sokolenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeatherTestTask/WeatherTestTask/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeatherTestTask
+//
+//  Created by Eugene Sokolenko on 25.04.15.
+//  Copyright (c) 2015 Eugene Sokolenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
